10e31c2cff379e3aebc8db905caebc378e60f107
aaqilafham <aaqilw@gmail.com>
2025-02-08 08:56:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
-resource "aws_instance" "webserver" {
-  ami = var.ami
+variable "ami" {
+  default = "ami-001e4d263e8980c84"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "region" {
+  default = "eu-west-2"
+}
+variable "webservers" {
+  type = list
+  default = ["web1","web2","web3"]
+}
+
+
+
+resource "aws_instance" "cerberus" {
+  ami           = var.ami
   instance_type = var.instance_type
-  count = 3
-  tags = {
-    Environment = "Production"
-   }
+  count =  length(var.webservers)
 }
