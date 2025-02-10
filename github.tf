548d9b92b6d4af465add048206384fe6b468b972
aaqilafham <aaqilw@gmail.com>
2025-02-10 19:46:35 +0530

    Update github.tf

--- github.tf
+++ github.tf
@@ -7,19 +7,7 @@ terraform {
   }
 }
 
-# Configure the GitHub Provider
+#Configure the GitHub Provider
 provider "github" {
-    token ="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"
-}
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
-
-  visibility = "public"
-
-  template {
-    owner                = "github"
-    repository           = "terraform-template-module"
-    include_all_branches = true
-  }
+    token ="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"
 }
